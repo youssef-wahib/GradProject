6dde30880b518d7673344a559e664a226fea4591
youssef-wahib <youssefwahib32@gmail.com>
2021-06-17 13:03:27 +0200

    work test

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -19,6 +19,7 @@ import {
 } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
@@ -50,146 +51,243 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 export default function Recommend() {
-  
-  let temp = [] 
-  useEffect(() => {
-    
-    let starCountRef = database.ref('Category/');
-  starCountRef.on('value', (snapshot) => {
-    const points = snapshot.val();
-    
-     
-     
-      for(let l in Object.keys(points)){
-        
-            temp.push( points[Object.keys(points)[l]])
-            
-      }
-      
-     console.log(temp) 
-  });
-  }, [])
-  
-  
-  function writeNewPost( categorie, body) {
+
+  // let temp = [];
+  // useEffect(() => {
+  //   let setRecommend = database.ref("Category/");
+  //   setRecommend.on("value", (snapshot) => {
+  //     const points = snapshot.val();
+  //     for (let l in Object.keys(points)) {
+  //       temp.push(points[Object.keys(points)[l]]);
+  //     }
+  //   });
+  // }, []);
+
+  const writeNewPost = (categorie, body) => {
     // A post entry.
-    var postData = {
-      
+    let postData = {
       Recommendation: body,
-      
     };
-  
+    let getRecommend = database.ref("Category/" + categorie);
+    getRecommend.on("value", (snapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+    });
+
     // Get a key for a new Post.
-    var newPostKey = database.ref().child('Category').push().key;
-  
+    let newPostKey = database.ref().child("Category").push().key;
+
     // Write the new post's data simultaneously in the posts list and the user's post list.
-    var updates = {};
-    updates['/Category/'+ categorie+ "/" + newPostKey] = postData;
-    
-  
+    let updates = {};
+    updates["/Category/" + categorie + "/" + newPostKey] = postData;
+
     return database.ref().update(updates);
-  }
+  };
   const Categories = [
-    "Excellent",
-    "Good",
     "Lightly Polluted",
     "Moderately Polluted",
     "Heavily Polluted",
     "Severely Polluted",
   ];
+
   const classes = useStyles();
-  const [Category, setCategory] = useState("Excellent");
+  const [Category, setCategory] = useState("Lightly Polluted");
   const [Recommendation, setRecommendation] = useState("");
+  const [LP, setLP] = useState({});
+  const [MP, setMP] = useState({});
+  const [HP, setHP] = useState({});
+  const [SP, setSP] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   
-    writeNewPost(Category,Recommendation)
+    writeNewPost(Category, Recommendation);
   };
 
+  useEffect(() => {
+    setLoading(true);
+    let readRecommend = database.ref("Category/");
+    readRecommend.on("value", (snapshot) => {
+      const data = snapshot.val();
+      setHP(Object.values(data)[0]);
+      setLP(Object.values(data)[1]);
+      setMP(Object.values(data)[2]);
+      setSP(Object.values(data)[3]);
+      setLoading(false);
+      console.log(LP)
+    });
+  }, [ loading ]);
+
+  // useEffect(() => {
+  //   const [key , val] = Object.entries(MP);
+  //   console.log({key});
+  //   console.log({val});
+  // }, [MP, SP, HP, LP]);
+
+
   return (
-    
-
-    <React.Fragment>
-      <Container maxWidth="lg" className={classes.container}>
-        <Grid container>
-          <Paper className={classes.paper}>
-            <Typography
-              variant="h5"
-              color="primary"
-              className={classes.titleSpace}
-            >
-              Create a New Recommendation
-            </Typography>
-            <form autoComplete="off" onSubmit={handleSubmit}>
-              <TextField
-                color="secondary"
-                id="newRecommendation"
-                label="New Recomendation"
-                variant="filled"
-                value={Recommendation}
-                onChange={(e) => setRecommendation(e.target.value)}
-                fullWidth
-                required
-                helperText="Please Write your Recommendation"
-              />
-              <FormControl className={classes.field}>
-                <InputLabel
-                  color="secondary"
-                  className={classes.selectCatLable}
-                  required
-                >
-                  Select Category
-                </InputLabel>
-
-                <Select
-                  id="Category"
-                  color="secondary"
-                  labelId="Select Category"
-                  value={Category}
-                  onChange={(e) => setCategory(e.target.value)}
-                  variant="filled"
-                  className={classes.selectEmpty}
-                >
-                  {Categories.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </Select>
-                <FormHelperText className={classes.selectCatLable}>
-                  Please select your Category
-                </FormHelperText>
-              </FormControl>
-              <Button
-                type="submit"
-                color="secondary"
-                variant="contained"
-                endIcon={<AddCircleOutlineIcon />}
+      <React.Fragment>
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container>
+            <Paper className={classes.paper}>
+              <Typography
+                  variant="h5"
+                  color="primary"
+                  className={classes.titleSpace}
               >
-                Add Recommendation
-              </Button>
-            </form>
-          </Paper>
-          <Paper className={classes.paper}>
-            <Typography
-              variant="h5"
-              color="primary"
-              className={classes.titleSpace}
-            >
-              List of Recommendations:
-            </Typography>
-            <List>
-              <ListItem>
-                
-                <IconButton edge="end" aria-label="delete">
-                  <RemoveCircleOutlineIcon />
-                </IconButton>
-              </ListItem>
-            </List>
-          </Paper>
-        </Grid>
-      </Container>
-    </React.Fragment>
+                Create a New Recommendation
+              </Typography>
+              <form autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                    color="secondary"
+                    id="newRecommendation"
+                    label="New Recommendation"
+                    variant="filled"
+                    value={Recommendation}
+                    onChange={(e) => setRecommendation(e.target.value)}
+                    fullWidth
+                    required
+                    helperText="Please Write your Recommendation"
+                />
+
+                <FormControl className={classes.field}>
+                  <InputLabel
+                      color="secondary"
+                      className={classes.selectCatLable}
+                      required
+                  >
+                    Select Category
+                  </InputLabel>
+
+                  <Select
+                      id="Category"
+                      color="secondary"
+                      labelId="Select Category"
+                      value={Category}
+                      onChange={(e) => setCategory(e.target.value)}
+                      variant="filled"
+                      className={classes.selectEmpty}
+                  >
+                    {Categories.map((option) => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                    ))}
+                  </Select>
+                  <FormHelperText className={classes.selectCatLable}>
+                    Please select your Category
+                  </FormHelperText>
+                </FormControl>
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    endIcon={<AddCircleOutlineIcon />}
+                >
+                  Add Recommendation
+                </Button>
+              </form>
+            </Paper>
+            <Paper className={classes.paper}>
+              <Typography
+                  variant="h5"
+                  color="primary"
+                  className={classes.titleSpace}
+              >
+                List of Recommendations:
+              </Typography>
+              <Typography
+                  variant="h6"
+                  color="secondary"
+                  className={classes.titleSpace}
+              >
+                Lightly Polluted:
+              </Typography>
+              <List>
+                {Object.keys(LP).length && Object.entries(LP).map(item => {
+                  const [[, id], [, {Recommendation: val}]] = Object.entries(item)
+                  return (
+                      <ListItem key={id}>
+                        <ArrowRightIcon/>   {val}
+                        <IconButton edge="end" aria-label="delete" onClick={() => {
+                          database.ref().child("/Category/Lightly Polluted/" + id).remove()
+
+                        } }>
+                          <RemoveCircleOutlineIcon />
+                        </IconButton>
+                      </ListItem>
+                  )
+                })}
+              </List>
+              <Typography
+                  variant="h6"
+                  color="secondary"
+                  className={classes.titleSpace}
+              >
+                Moderately Polluted:
+              </Typography>
+              <List>
+                {Object.keys(MP).length && Object.entries(MP).map(item => {
+                  const [[, id], [, {Recommendation: val}]] = Object.entries(item)
+                  return (
+                      <ListItem key={id}>
+                        <ArrowRightIcon/>    {val}
+                        <IconButton edge="end" aria-label="delete" onClick={() => {
+                          database.ref().child("/Category/Moderately Polluted/" + id).remove()
+
+                        } }>
+                          <RemoveCircleOutlineIcon />
+                        </IconButton>
+                      </ListItem>
+                  )
+                })}
+              </List>
+              <Typography
+                  variant="h6"
+                  color="secondary"
+                  className={classes.titleSpace}
+              >
+                Heavily Polluted:
+              </Typography>
+              <List>
+                {Object.keys(HP).length && Object.entries(HP).map(item => {
+                  const [[, id], [, {Recommendation: val}]] = Object.entries(item)
+                  return (
+                      <ListItem key={id}>
+                        <ArrowRightIcon/>        {val}
+                        <IconButton edge="end" aria-label="delete" >
+                          <RemoveCircleOutlineIcon />
+                        </IconButton>
+                      </ListItem>
+                  )
+                })}
+              </List>
+              <Typography
+                  variant="h6"
+                  color="secondary"
+                  className={classes.titleSpace}
+              >
+                Severely Polluted:
+              </Typography>
+              <List>
+                {Object.keys(SP).length && Object.entries(SP).map(item => {
+                  const [[, id], [, {Recommendation: val}]] = Object.entries(item)
+                  return (
+                      <ListItem key={id}>
+                        <ArrowRightIcon/>    {val}
+                        <IconButton edge="end" aria-label="delete" onClick={() => {
+                          database.ref().child("/Category/Severely Polluted/" + id).remove()
+
+                        } }>
+                          <RemoveCircleOutlineIcon />
+                        </IconButton>
+                      </ListItem>
+                  )
+                })}
+              </List>
+            </Paper>
+          </Grid>
+        </Container>
+      </React.Fragment>
   );
 }
