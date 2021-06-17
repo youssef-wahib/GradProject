448e5959777c0bdb9796adc022cf723f16e56231
youssef-wahib <youssefwahib32@gmail.com>
2021-06-17 13:18:58 +0200

    work test

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,97 +3,72 @@ import { useTheme } from "@material-ui/core/styles";
 import {database} from "../state/firebase"
 import {
   LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  Label,
-  ResponsiveContainer,
+      Line,
+      XAxis,
+      YAxis,
+      Label,
+      CartesianGrid,
+      ResponsiveContainer,
 } from "recharts";
 import Title from "./Title";
 import { useStore } from "../state/state";
-
-// useEffect(() => {
-
-// }, [])
-// Generate Sales Data
-
-
-
-
 function createData(time, amount) {
   return { time, amount };
 }
-
-
-
-const data = [
-  createData("00:00", 110),
-  createData("03:00", 300),
-  createData("06:00", 200),
-  createData("09:00", 150),
-  createData("12:00", 210),
-  createData("15:00", 200),
-  createData("18:00", 260),
-  createData("21:00", 180),
-  createData("24:00", undefined),
-];
-
 export default function Chart() {
+
   const theme = useTheme();
-  const {graph, setGraph} = useStore()
-  
-  
-  let temp = [] 
+  const { graph, setGraph } = useStore();
+
+  let temp = [];
   useEffect(() => {
-    
-    let starCountRef = database.ref('Readings/');
-  starCountRef.on('value', (snapshot) => {
-    const points = snapshot.val();
-    
-     
-     
-      for(let l in Object.keys(points)){
-        
-            temp.push(createData(Object.keys(points)[l], points[Object.keys(points)[l]]))
-            
+    let starCountRef = database.ref("Readings/");
+    starCountRef.on("value", (snapshot) => {
+      const points = snapshot.val();
+      for (let l in Object.keys(points)) {
+        temp.push(
+            createData(Object.keys(points)[l], points[Object.keys(points)[l]])
+        );
       }
-      setGraph(temp)
-      
-  });
-  }, [])
-  
-  
+      setGraph(temp);
+    });
+  }, []);
+
   return (
-    <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={graph}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-          <YAxis stroke={theme.palette.text.secondary}>
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: "middle", fill: theme.palette.text.primary }}
-            >
-              AQI Level
-            </Label>
-          </YAxis>
-          <Line
-            type="monotone"
-            dataKey="amount"
-            stroke={theme.palette.primary.main}
-            dot={false}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
+      <React.Fragment>
+        <Title>Today</Title>
+        <ResponsiveContainer>
+          <LineChart
+              data={graph}
+              margin={{
+                top: 16,
+                right: 16,
+                bottom: 0,
+                left: 24,
+              }}
+          >
+            <XAxis dataKey="time" stroke={theme.palette.grey[900]} />
+            <YAxis stroke={theme.palette.grey[900]}>
+              <Label
+                  angle={270}
+                  position="left"
+                  style={{ textAnchor: "middle", fill: theme.palette.text.primary }}
+              >
+                AQI Level
+              </Label>
+            </YAxis>
+            <CartesianGrid
+                stroke={theme.palette.grey[200]}
+                strokeDasharray="5 5"
+            />
+            <Line
+                type="monotone"
+                dataKey="amount"
+                stroke={theme.palette.primary.main}
+                dot={true}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </React.Fragment>
   );
 }
