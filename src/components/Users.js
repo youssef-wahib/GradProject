b577cb4ba66e619f896bc242533d34250e335857
youssef-wahib <youssefwahib32@gmail.com>
2021-07-07 17:20:12 +0200

    work test

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,18 @@
 import React from "react";
 // import { database } from "../state/firebase";
-import Chart from "./Chart";
+import { useTheme } from "@material-ui/core/styles";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Label,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+import { useStore } from "../state/state";
 import {
   makeStyles,
   Container,
@@ -8,7 +20,6 @@ import {
   Grid,
   Typography,
 } from "@material-ui/core";
-import { useStore } from "../state/state";
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
@@ -24,6 +35,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Rooms = ["Emergency unit", "Nursing room", "Patient rooms", "Reception"];
 export default function Users() {
+  const theme = useTheme();
   const classes = useStyles();
   const { Selected } = useStore();
   return (
@@ -35,7 +47,52 @@ export default function Users() {
               <Typography variant="h4" color="secondary">
                 Predictions for {Rooms[Selected]}:
               </Typography>
-              <Chart />
+              <ResponsiveContainer>
+                <LineChart
+                  // data={graph}
+                  margin={{
+                    top: 16,
+                    right: 16,
+                    bottom: 0,
+                    left: 24,
+                  }}
+                >
+                  <XAxis dataKey="time" stroke={theme.palette.grey[900]}>
+                    <Label
+                      angle={0}
+                      position="Center"
+                      style={{
+                        textAnchor: "middle",
+                        fill: theme.palette.text.primary,
+                      }}
+                    >
+                      Predicted Time(h)
+                    </Label>
+                  </XAxis>
+                  <YAxis stroke={theme.palette.grey[900]}>
+                    <Label
+                      angle={270}
+                      position="left"
+                      style={{
+                        textAnchor: "middle",
+                        fill: theme.palette.text.primary,
+                      }}
+                    >
+                      AQI Level
+                    </Label>
+                  </YAxis>
+                  <CartesianGrid
+                    stroke={theme.palette.grey[200]}
+                    strokeDasharray="5 5"
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey="amount"
+                    stroke={theme.palette.primary.main}
+                    dot={true}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
             </Paper>
           </Grid>
         </Grid>
